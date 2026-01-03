9abea9731a1e31dd5744f4c7c47cfc8b5e85cfaf
rady112 <rady_h_@hotmail.com>
2026-01-03 04:41:41 +0200

    Update shots.js

--- data/shots.js
+++ data/shots.js
@@ -1,1 +1,10 @@
-
+export default [
+  { id: "sh1", name: "Red Label", ingredients: [] },
+  { id: "sh2", name: "Blue Label", ingredients: [] },
+  { id: "sh3", name: "Green Label", ingredients: [] },
+  { id: "sh4", name: "Black Label", ingredients: [] },
+  { id: "sh5", name: "Tequila Blanco", ingredients: [] },
+  { id: "sh6", name: "Tequila Reposado", ingredients: [] },
+  { id: "sh7", name: "Tequila Añejo", ingredients: [] },
+  { id: "sh8", name: "Vodka", ingredients: [] },
+];
