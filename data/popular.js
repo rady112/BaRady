72824fe2254aa33f4b8cedb66eab5656a2b4aad9
rady112 <rady_h_@hotmail.com>
2026-01-03 04:41:00 +0200

    Update popular.js

--- data/popular.js
+++ data/popular.js
@@ -1,1 +1,37 @@
-
+export default [
+  {
+    id: "pc1",
+    name: "Aperol Spritz",
+    ingredients: ["Aperol", "Prosecco", "soda water", "orange slice", "ice"],
+  },
+  {
+    id: "pc2",
+    name: "Cosmopolitan",
+    ingredients: ["vodka", "triple sec", "cranberry juice", "lime juice"],
+  },
+  {
+    id: "pc3",
+    name: "Espresso Martini",
+    ingredients: ["vodka", "coffee liqueur", "espresso", "simple syrup (optional)"],
+  },
+  {
+    id: "pc4",
+    name: "Hugo Spritz",
+    ingredients: ["Prosecco", "elderflower syrup/liqueur", "soda water", "mint", "lime", "ice"],
+  },
+  {
+    id: "pc5",
+    name: "Kamikaze",
+    ingredients: ["vodka", "triple sec", "lime juice"],
+  },
+  {
+    id: "pc6",
+    name: "Long Island",
+    ingredients: ["vodka", "gin", "white rum", "tequila", "triple sec", "lemon juice", "cola"],
+  },
+  {
+    id: "pc7",
+    name: "Sex on the Beach",
+    ingredients: ["vodka", "peach schnapps", "orange juice", "cranberry juice"],
+  },
+];
